86ab028d963193b46834c09b39539bfd156cd0a1
Jana <jana.walfort@tobit.com>
2021-01-11 09:27:05 +0100

    Initial commit

---
+++ SUD_TextAdventure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SUD_TextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
