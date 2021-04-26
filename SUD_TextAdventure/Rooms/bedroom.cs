2420a9414139bdf5cb2ea3f93b09a9bc6f29e369
Jana <jana.walfort@tobit.com>
2021-04-26 09:24:06 +0200

    🚧

--- SUD_TextAdventure/Rooms/bedroom.cs
+++ SUD_TextAdventure/Rooms/bedroom.cs
@@ -14,8 +14,27 @@ namespace SUD_TextAdventure.Rooms
         public string run()
         {
             // TODO: Write 2
-            Console.WriteLine("Hello");
-            Console.Read();
+            Console.WriteLine("Du bist zurück im Schlafzimmer");
+            Console.WriteLine("Du siehst: Einen Schrank, ein Bett und eine Tür. Was willst Du Dir genauer ansehen?");
+            var validInput = new[] {"tür", "schrank", "bett", "zurück"};
+            var confirmation = _Program.CheckInput(validInput);
+            switch (confirmation)
+            {
+                case "tür":
+                    // ToDo: Add text
+                    break;
+                case "schrank":
+                    // ToDo: Add text
+                    break;
+                case "bett":
+                    // ToDo: Add text
+                    break;
+                case "zurück":
+                    // ToDo: Add text
+                    return "corridorOne";
+            }
+            
+            
             return "";
         }
     }
--- SUD_TextAdventure/Rooms/corrdidor2.cs
+++ SUD_TextAdventure/Rooms/corrdidor2.cs
@@ -1,4 +1,6 @@
-﻿namespace SUD_TextAdventure.Rooms
+﻿using System;
+
+namespace SUD_TextAdventure.Rooms
 {
     public class corrdidor2
     {
@@ -13,6 +15,26 @@
         public string run()
         {
             // TODO: Write 2
+            Console.WriteLine("Du befindest Dich in einem langen Korridor, der ein paar Meter vor Dir eine Kurve macht.");
+            var validInput = new[] {"umsehen", "rot", "braun", "marmor"};
+            var confirmation = _Program.CheckInput(validInput);
+            switch (confirmation)
+            {
+                case "umsehen":
+                    Console.WriteLine("Dieser Korridor scheint nicht viel anders als der Andere. Hier und da steht etwas Dekoration, der schon länger nicht mehr geputzt wurde.");
+                    Console.WriteLine("Du gehst um die Ecke und erkennst, dass der Korridor insgesamt zu drei Räumen führt.");
+                    Console.WriteLine("Die braune Tür führt zurück zur Eingangshalle. Die zweite Tür wirkt ziemlich imposant und ist komplett aus Marmor gemacht. Die Dritte Tür hat einen rötlicheren Ton als die Türen, die Du bis jetzt gesenen hast.");
+                    break;
+                case "rot":
+                    // ToDo: Add text
+                    return "libarary";
+                case "braun":
+                    // ToDo: Add text
+                    return "entranceArea";
+                case "mamor":
+                    // ToDo: Add text
+                    return "ballroom";
+            }
 
             return "";
         }
