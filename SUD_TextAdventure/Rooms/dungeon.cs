493cc05e8bf60668099fcd696422139d532f33bf
Jana <jana.walfort@tobit.com>
2021-06-20 18:43:00 +0200

    🚧 Update dungeon

--- SUD_TextAdventure/Rooms/dungeon.cs
+++ SUD_TextAdventure/Rooms/dungeon.cs
@@ -24,9 +24,10 @@ namespace SUD_TextAdventure.Rooms
                 Console.WriteLine("Nur durch das bisschen Licht was von oben herab scheint, erkennst Du, dass sich der enge Korridor in zwei Wege aufteilt, die tiefer in die Dunkelheit führen.");
             }
             Console.WriteLine("Dir läuft es eiskalt den Rücken herunter und Du musst plötzlich an all die alten Horrorspiele denken, die Du schon einmal gesehen hast.");
-
-
-            return "";
+            
+            // ToDo: Add continuation
+            Console.WriteLine("Vor lauter Angst entscheidest Du Dich wieder die Treppe hochzulaufen.");
+            return "corridorTwo";
         }
     }
 }
