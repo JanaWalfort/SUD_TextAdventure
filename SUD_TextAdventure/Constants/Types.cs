befec23ee942733cb14f490fe6c8ea47cffdad4e
Jana <jana.walfort@tobit.com>
2021-06-20 15:58:30 +0200

    ✨ Add enum for note distribution

---
+++ SUD_TextAdventure/Constants/Types.cs
@@ -0,0 +1,20 @@
+﻿namespace SUD_TextAdventure.Constants
+{
+	public static class Types
+	{
+		public enum NoteRoom
+		{
+			Attic = 1,
+			Balcony = 2,
+			Ballroom = 3,
+			Bedroom = 4,
+			Corridor2 = 5,
+			Corridor1 = 6,
+			Dungeon = 7,
+			EntranceArea = 8,
+			Garden = 9,
+			Kitchen = 10,
+			Library = 11
+		}
+	}
+}
